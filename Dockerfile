a8a4d380b7ed8047ef242dd0cd974870298f84e5
Xpl0itU <samuelcarballogarcia8@gmail.com>
2023-01-19 16:18:57 +0100

    Update for aroma

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,5 @@
-FROM wiiuenv/devkitppc:20220710 AS final
+FROM wiiuenv/devkitppc:20220907
 
-ENV WUT_ROOT=$DEVKITPRO/wut
-
-WORKDIR /
-RUN git clone --single-branch https://github.com/Maschell/WiiUPluginSystem && \
- cd WiiUPluginSystem && \
- make -j$(nproc) && \
- make install && \
- cd .. && \
- rm -rf WiiUPluginSystem
+COPY --from=wiiuenv/wiiupluginsystem:20220904 /artifacts $DEVKITPRO
 
 WORKDIR /project
--- src/main.cpp
+++ src/main.cpp
@@ -5,23 +5,15 @@
 #include <nn/acp/drcled_c.h>
 #include <nn/act/client_cpp.h>
 
-static uint32_t pId;
-
-/**
-    Mandatory plugin information.
-    If not set correctly, the loader will refuse to use the plugin.
-**/
-WUPS_PLUGIN_NAME("DRC LED");
-WUPS_PLUGIN_DESCRIPTION("This plugin turns on the GamePad LED.");
+WUPS_PLUGIN_NAME("drc-led");
+WUPS_PLUGIN_DESCRIPTION("Turns on LED on the GamePad");
 WUPS_PLUGIN_VERSION("v1.0");
 WUPS_PLUGIN_AUTHOR("DaThinkingChair");
 WUPS_PLUGIN_LICENSE("GPLv3");
 
-WUPS_FS_ACCESS()
+static uint32_t pId;
 
-WUPS_ALLOW_OVERLAY()
-
-INITIALIZE_PLUGIN(){
+INITIALIZE_PLUGIN() {
     pId = nn::act::GetPersistentId();
     ACPTurnOnDrcLed(pId, 1);
 }
